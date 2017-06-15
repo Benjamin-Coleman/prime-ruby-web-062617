1bf5f5e71b3832cc8af6f50b02d76d0ed49db5ef
Benjamin Coleman <hello@benjamincoleman.me>
2017-06-15 21:22:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  array = (2...num).to_a
+  final = []
+  array.each do |number|
+    if num % number == 0
+      final.push(number)
+    end
+  end
+  return final.count == 0
+end
+
+#prime?(7)
+#prime?(6)
